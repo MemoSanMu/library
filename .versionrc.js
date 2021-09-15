771d7900f24efba76b6c809135aa603be559fa58
王森 <wangsen1@yonghui.cn>
2021-09-15 14:52:27 +0800

    feat: [ws][2021-09-15][init all]

---
+++ .versionrc.js
@@ -0,0 +1,64 @@
+module.exports = {
+  types: [
+    {
+      type: 'chore',
+      hidden: false,
+      section: '🚀 Chore | 构建/工程依赖/工具',
+    },
+    {
+      type: 'revert',
+      hidden: false,
+      section: '⏪ Revert | 回退',
+    },
+    {
+      type: 'feat',
+      hidden: false,
+      section: '✨ Features | 新功能',
+    },
+    {
+      type: 'fix',
+      hidden: false,
+      section: '🐛 Bug Fixes | Bug 修复',
+    },
+    {
+      type: 'improvement',
+      hidden: false,
+      section: '✏️ improvement | 改进',
+    },
+    {
+      type: 'docs',
+      hidden: false,
+      section: '📝 Documentation | 文档',
+    },
+    {
+      type: 'style',
+      hidden: false,
+      section: '💄 Styles | 风格样式',
+    },
+    {
+      type: 'refactor',
+      hidden: false,
+      section: '♻️ Code Refactoring | 代码重构',
+    },
+    {
+      type: 'perf',
+      hidden: false,
+      section: '⚡ Performance Improvements | 性能优化',
+    },
+    {
+      type: 'test',
+      hidden: false,
+      section: '✅ Tests | 测试',
+    },
+    {
+      type: 'build',
+      hidden: false,
+      section: '📦‍ Build System | 打包构建',
+    },
+    {
+      type: 'ci',
+      hidden: false,
+      section: '👷 Continuous Integration | CI 配置',
+    },
+  ],
+};
